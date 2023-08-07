2e2c62801d88edd25f63bf10f8c96c74e2d1720e
20221PF.CC0007 <20221PF.CC0007@PC-155976.passofundo.ifsul.local>
2023-08-07 11:41:13 -0300

    projeto inicial da aula de 07/08/2023.

---
+++ src/main/java/br/edubr/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_londero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edubr.edu.ifsul.bcc.too.exercicios.respostas;
+
+/**
+ *
+ * @author 20221PF.CC0007
+ */
+public class Java_maven_too_londero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
